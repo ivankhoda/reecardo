ca1840c922380258e3547306a1950ab5e9a796b7
Ivan Khoda <ivankhoda@gmail.com>
2022-06-17 23:37:36 +0300

    Experiments

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,17 @@
 class Card < ApplicationRecord
   belongs_to :user
   has_many :vendors
+  attr_reader :codegenerator
+
+  def initialize(codegenerator)
+    @codegenerator = codegenerator
+  end
 
   def self.barcode(codetype, value)
     Codetype.print_code(codetype, value)
+  end
+
+  def self.code
+    codegenerator.print_code
   end
 end
