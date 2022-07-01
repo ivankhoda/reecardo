4faea2da2e06a50c6e2ef4e70e478226a4ebded9
Ivan Khoda <ivankhoda@gmail.com>
2022-07-01 21:19:05 +0300

    Test context

--- app/helpers/callback_query_answer_helper.rb
+++ app/helpers/callback_query_answer_helper.rb
@@ -11,7 +11,8 @@ module CallbackQueryAnswerHelper
 
     when 'add_new_card'
       save_context :new_card
-      respond_with :message, text: 'Пожалуйста, укажите название магазина и код карточки.'
+      reply_with :message, text: 'Пожалуйста, укажите название магазина и код карточки.'
+
     else
       reply_with :message, text: 'Not found command'
     end
